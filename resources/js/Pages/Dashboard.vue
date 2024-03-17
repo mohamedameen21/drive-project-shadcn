ad12b8a5ffb064e7da1d23fc31db3460a3c9be25
Ameen <mohamedameen.21.2001@outlook.com>
2024-03-17 10:08:48 +0530

    search ui and basic dropdown completed.

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,9 +4,21 @@ import { Head } from "@inertiajs/vue3";
 import Input from "@/shadcn/ui/input/Input.vue";
 import { useColorMode } from "@vueuse/core";
 import { ref } from "vue";
-const mode = useColorMode();
 
-const search = ref("");
+import { Button } from "@/shadcn/ui/button";
+import { Search, Folder, FolderPlus, CloudUpload, Plus } from "lucide-vue-next";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/shadcn/ui/dropdown-menu";
+
+const mode = useColorMode();
+const searchedTerm = ref("");
 </script>
 
 <template>
@@ -22,16 +34,76 @@ const search = ref("");
         <!--        </template>-->
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="max-w-[90rem] mx-auto sm:px-6 lg:px-8">
                 <div
                     class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"
                 >
                     <div class="p-6 text-gray-900 dark:text-gray-100">
-                        <Input
-                            placeholder="Search for Files and Folders"
-                            class="py-6 text-base"
-                            v-model="search"
-                        />
+                        <main
+                            class="flex flex-col gap-6 justify-center items-center sm:flex-row"
+                        >
+                            <!--                      Dropdown button-->
+                            <div class="w-full sm:max-w-sm">
+                                <DropdownMenu
+                                    :defaultOpen="true"
+                                    class="w-72 sm:w-96"
+                                >
+                                    <DropdownMenuTrigger
+                                        class="w-72 sm:w-96"
+                                        as-child
+                                    >
+                                        <Button
+                                            class="w-full py-6"
+                                            variant="outline"
+                                        >
+                                            <Plus class="mr-2 h-4 w-4" />
+                                            Create New</Button
+                                        >
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent class="w-72 sm:w-96">
+                                        <DropdownMenuLabel
+                                            >Create New
+                                        </DropdownMenuLabel>
+                                        <DropdownMenuSeparator />
+                                        <DropdownMenuGroup>
+                                            <DropdownMenuItem>
+                                                <Folder class="mr-2 h-4 w-4" />
+                                                <span>New Folder</span>
+                                            </DropdownMenuItem>
+                                            <DropdownMenuItem>
+                                                <CloudUpload
+                                                    class="mr-2 h-4 w-4"
+                                                />
+                                                <span>Upload File</span>
+                                            </DropdownMenuItem>
+                                            <DropdownMenuItem>
+                                                <FolderPlus
+                                                    class="mr-2 h-4 w-4"
+                                                />
+                                                <span>Upload Folder</span>
+                                            </DropdownMenuItem>
+                                        </DropdownMenuGroup>
+                                    </DropdownMenuContent>
+                                </DropdownMenu>
+                            </div>
+
+                            <!--                      Search component-->
+                            <div class="relative w-full items-center">
+                                <Input
+                                    id="search"
+                                    type="text"
+                                    placeholder="Search for files and folder"
+                                    class="pl-12 py-6 text-base"
+                                />
+                                <span
+                                    class="absolute start-0 inset-y-0 flex items-center justify-center px-2"
+                                >
+                                    <Search
+                                        class="size-6 text-muted-foreground"
+                                    />
+                                </span>
+                            </div>
+                        </main>
                     </div>
                 </div>
             </div>
